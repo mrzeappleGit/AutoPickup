968fcb5ccf0cb3823dd3690fa768cbc9f157a56a
phillip <phillip@philderbeast.com>
2017-03-03 03:54:00 +1100

    readd full notify

---
+++ src/main/java/me/MnMaxon/AutoPickup/commands/FullNotify.java
@@ -0,0 +1,49 @@
+package me.MnMaxon.AutoPickup.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.MnMaxon.AutoPickup.AutoPickupPlugin;
+import me.MnMaxon.AutoPickup.Message;
+
+public class FullNotify implements CommandExecutor
+{
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if(cmd.getName().equals("AutoPickup"))
+        {
+            Player p = (Player)sender; 
+            if (!p.hasPermission("FullNotify.command")) 
+            {
+                p.sendMessage(Message.ERROR0NO_PERM + "");
+            }else if (args.length == 0)
+            {
+                p.sendMessage(ChatColor.RED + "Use like: /FullNotify toggle");
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("toggle"))
+            {
+                if (!p.hasPermission("FullNotify.toggle")) 
+                {
+                    p.sendMessage(Message.ERROR0NO_PERM + "");
+                } else if (AutoPickupPlugin.fullNotify.contains(p.getName())) 
+                {
+                    AutoPickupPlugin.fullNotify.remove(p.getName());
+                    p.sendMessage(Message.SUCCESS0TOGGLE0NOTIFY_OFF + "");
+                } else 
+                {
+                    AutoPickupPlugin.fullNotify.add(p.getName());
+                    p.sendMessage(Message.SUCCESS0TOGGLE0NOTIFY_ON + "");
+                }
+            }else
+            {
+                Common.displayHelp(p);
+            } 
+        }
+        return false;
+    }
+
+}
