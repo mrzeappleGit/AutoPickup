65505aef7db8c4dfc4e8b9716df91ff3ed6b42f4
phillip <phillip@philderbeast.com>
2017-03-03 02:36:45 +1100

    Mythic Mob Listener Clenaup

--- src/main/java/me/MnMaxon/AutoPickup/MythicMobListener.java
+++ src/main/java/me/MnMaxon/AutoPickup/MythicMobListener.java
@@ -1,34 +1,62 @@
-package me.MnMaxon.AutoPickup;
+package me.MnMaxon.AutoPickup; 
 
-import net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
+import net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent; 
+import org.bukkit.entity.Player; 
+import org.bukkit.event.EventHandler; 
+import org.bukkit.event.Listener; 
+import org.bukkit.inventory.ItemStack; 
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.ArrayList; 
+import java.util.HashMap; 
 
 /**
  * Created by MnMaxon on 2/16/2016.  Aren't I great?
  */
-public class MythicMobListener implements Listener {
+public class MythicMobListener implements Listener 
+{
+
     @EventHandler(ignoreCancelled = true)
-    public void onKill(MythicMobDeathEvent e) {
-        if (e.getKiller() == null || !(e.getKiller() instanceof Player)) return;
-        Player killer = (Player) e.getKiller();
-        if (Config.getBlockedWorlds().contains(killer.getWorld()))
+    public void onKill(MythicMobDeathEvent e)
+    {
+        if (e.getKiller() == null || !(e.getKiller() instanceof Player)) 
+        {
             return;
-        if (Config.autoMob) {
-            ArrayList<ItemStack> newDrops = new ArrayList<>();
-            for (ItemStack drop : e.getDrops()) {
-                HashMap<Integer, ItemStack> remaining = killer.getInventory().addItem(drop);
-                for (ItemStack remainder : remaining.values()) newDrops.add(remainder);
-            }
-            if (!newDrops.isEmpty()) AutoPickupPlugin.warn(killer);
-            e.getDrops().clear();
-            if (!Config.deleteOnFull) for (ItemStack is : newDrops) e.getDrops().add(is);
         }
+
+        Player killer = (Player) e.getKiller();
+
+        if (Config.getBlockedWorlds().contains(killer.getWorld()))
+        {
+            return;
+        }
+
+        if (Config.autoMob)
+        {
+            ArrayList<ItemStack> newDrops = new ArrayList<>();
+            for (ItemStack drop : e.getDrops()) 
+            {
+                HashMap<Integer, ItemStack> remaining = killer.getInventory().addItem(drop);
+                for (ItemStack remainder : remaining.values())
+                {
+                    newDrops.add(remainder);
+                }
+            }
+
+            if (!newDrops.isEmpty())
+            {
+                AutoPickupPlugin.warn(killer);
+            }
+
+            e.getDrops().clear();
+            if (!Config.deleteOnFull) 
+            {
+                for (ItemStack is : newDrops)
+                {
+                    e.getDrops().add(is);
+                }
+            }
+        }
+        
         if (Config.autoMobXP) {
             killer.giveExp(e.getExp());
             e.setExp(0);
