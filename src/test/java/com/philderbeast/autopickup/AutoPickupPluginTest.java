89b143f69666492f27f8920add11c70d53e720b0
Phillip Ledger <phillip@philderbeast.com>
2019-12-22 13:20:41 +1100

    save local

--- src/test/java/com/philderbeast/autopickup/AutoPickupPluginTest.java
+++ src/test/java/com/philderbeast/autopickup/AutoPickupPluginTest.java
@@ -3,32 +3,22 @@ package com.philderbeast.autopickup;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.function.Predicate;
-
-import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import be.seeseemelk.mockbukkit.ServerMock;
-import be.seeseemelk.mockbukkit.entity.PlayerMock;
-import be.seeseemelk.mockbukkit.plugin.PluginManagerMock;
 import be.seeseemelk.mockbukkit.MockBukkit;
 
 public class AutoPickupPluginTest {
 
 	private ServerMock server;
-    private AutoPickupPlugin plugin;
-	private PluginManagerMock pluginManager;
 
     @Before
     public void setUp()
     {
         server = MockBukkit.mock();
-        pluginManager = server.getPluginManager();
-        plugin = (AutoPickupPlugin) MockBukkit.load((Class<AutoPickupPlugin>) AutoPickupPlugin.class);
+        MockBukkit.load(AutoPickupPlugin.class);
     }
 
     @After
@@ -40,24 +30,24 @@ public class AutoPickupPluginTest {
     @Test
     public void dummyTest()
     {
-        assertTrue(true);
+        Assert.assertTrue(true);`
     }
 
     @Test
     public void playerJoin()
     {
-        PlayerMock player = server.addPlayer();
+        server.addPlayer();
 
         // this failes due to an error int he mocking library
-        // pluginManager.assertEventFired(event -> { return event instanceof PlayerJoinEvent; });
+        server.getPluginManager().assertEventFired(event -> { return event instanceof PlayerJoinEvent; });
 
         // //no permissions so they arnt added
-        // assertFalse(AutoPickupPlugin.autoBlock.contains(player.getName()));
-        // assertFalse(AutoPickupPlugin.autoPickup.contains(player.getName()));
-        // assertFalse(AutoPickupPlugin.autoSmelt.contains(player.getName()));
+        // Assert.assertFalse(AutoPickupPlugin.autoBlock.contains(player.getName()));
+        // Assert.assertFalse(AutoPickupPlugin.autoPickup.contains(player.getName()));
+        // Assert.assertFalse(AutoPickupPlugin.autoSmelt.contains(player.getName()));
 
         // server.execute("autopickup", player);
-        // assertTrue(AutoPickupPlugin.autoPickup.contains(player.getName()));
+        // Assert.assertTrue(AutoPickupPlugin.autoPickup.contains(player.getName()));
     }
 
 
